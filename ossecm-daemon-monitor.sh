099702cd232ffb64c07f0ece7c76c740d11fe166
Tony Perez <tony@perezbox.com>
2021-05-27 13:09:27 -0500

    adding new monitoring script for ossec daemons

---
+++ ossecm-daemon-monitor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Monitor state of OSSEC agents
+# Check all Core OSSEC Daemons are running
+# If daemons are not running -> Recrod in a new log and notify via Slack Integration
+
+#/var/ossec/bin/ossec-control status | grep “not running…” >/dev/null 2>&1
+
+echo "This script is designed to be used with a cron job."
+echo "It checks to make sure that the OSSEC daemons are running. If not, they send a notification to your slack."
+
+echo "Enter Slack Channel:"
+
+read channel
+
+echo "Enter alert title:"
+
+read alerttitle
+
+echo "Enter full slack webhook:"
+
+read slackhook
+
+for i in ossec-execd ossec-remoted ossec-analysisd ossec-syscheckd ossec-monitord ossec-logcollector ossec-integratord; 
+	 
+do ps auwx | grep -v grep | grep $i >/dev/null 2>&1 ; 
+
+if [ $? = 0 ]; 
+			       
+then 
+echo `date "+%Y-%m-%d %H:%M "`"$i Running...";
+					          
+
+else
+	
+echo `date "+%Y-%m-%d %H:%M "`"$i not running..."; >> /root/documents/logs/ossecm/ossecm-script.log
+curl -X POST --data-urlencode "payload={\"channel\": \"#$channel\", \"username\": \"$ORG OSSECM $date:\", \"text\": \"$alerttitle: $i Daemons disabled. Remediation required.\", \"icon_emoji\": \":ghost:\"}" $slackhook
+
+fi;
+done
